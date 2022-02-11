0dc78fa2fbab2b669b815502a0ea0ecc5b716f49
Kerem <kerem@cankod.com>
2022-02-11 12:06:07 +0300

    Hello World

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,33 +7,29 @@ type Props = {
   title?: string
 }
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
-  <div>
+const Layout = ({ children, title = 'Intool Dev' }: Props) => (
+  <div className="flex flex-col h-screen bg-white dark:bg-neutral-900">
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-    <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/users">
-          <a>Users List</a>
-        </Link>{' '}
-        | <a href="/api/users">Users API</a>
-      </nav>
-    </header>
-    {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
+    <div className="container mx-auto md:pt-5 lg:pt-20 px-4 max-w-2xl text-center flex-grow">
+      <Link href="/">
+        <h1 className="text-6xl md:text-8xl tracking-tighter leading-tight text-gray-700 dark:text-neutral-700 cursor-pointer">in<span className="font-bold dark:text-gray-800">tool</span></h1>
+      </Link>
+      <div className="mt-10">
+        <form className="flex flex-wrap items-center justify-center">
+          <input className="w-full px-4 py-3 text-gray-700 dark:text-white bg-gray-100 dark:bg-neutral-800 focus:outline-gray-200 dark:focus:outline-neutral-600 focus:bg-white dark:focus:bg-neutral-800 focus:border-gray-500 dark:focus:border-neutral-700" type="text" placeholder="Search" />
+        </form>
+
+        <div>
+          {children}
+        </div>
+      </div>
+    </div>
+  <footer className="justify-center items-center text-center">
+      <span>Made by CANKOD</span>
     </footer>
   </div>
 )
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,15 @@ import Link from 'next/link'
 import Layout from '../components/Layout'
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
+  <Layout title="Intool Developer">
+    <div className='my-3 flex flex-wrap -m-1'>
+        <Link href="/about"><span className="m-1 bg-gray-200 hover:bg-gray-300 rounded-full px-2 font-bold text-sm leading-loose cursor-pointer">#username-regex</span></Link>
+        <span className="m-1 bg-gray-200 hover:bg-gray-300 rounded-full px-2 font-bold text-sm leading-loose cursor-pointer">#stark</span>
+        <span className="m-1 bg-gray-200 hover:bg-gray-300 rounded-full px-2 font-bold text-sm leading-loose cursor-pointer">#gameofthrones</span>
+        <span className="m-1 bg-gray-200 hover:bg-gray-300 rounded-full px-2 font-bold text-sm leading-loose cursor-pointer">#battle</span>
+        <span className="m-1 bg-gray-200 hover:bg-gray-300 rounded-full px-2 font-bold text-sm leading-loose cursor-pointer">#jhonsnow</span>
+        <span className="m-1 bg-gray-200 hover:bg-gray-300 rounded-full px-2 font-bold text-sm leading-loose cursor-pointer">#kinglandings</span>
+    </div>
   </Layout>
 )
 
